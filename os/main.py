8cbc9a56685f902477c86efd21244fe77d7ed203
muradcoder1 <huseyinovai40@gmail.com>
2024-03-06 15:51:48 +0400

    Finish

--- os/main.py
+++ os/main.py
@@ -4,7 +4,17 @@ def list_directiry(path="."):
      print("Contents of directiry")
      for item in os.listdir():
            print(item)
-
+def create_directiry(directiry_name):
+    try:
+            
+            os.makedirs(directiry_name)
+    except FileExistsError:
+            print("This file is exists please try another")
+def remove_directory(directiry_name):
+     try:
+            os.rmdir(directiry_name)
+     except FileNotFoundError:
+            print("There is not the file")
 def main():
     print("1. List contents of current directory")
     print("2. Create a new directory")
@@ -15,19 +25,12 @@ def main():
         
        
     elif choice=="2":
-         filename=input("filemame")
-         try:
-            
-            os.makedirs(filename)
-         except FileExistsError:
-            print("This file is exists please try another")
+        filename=input("filemame")
+        create_directiry(filename)
     elif choice=="3":
         filename=input("filename")
-        try:
-            
-            os.rmdir(filename)
-        except FileNotFoundError:
-            print("There is not the file")
+        remove_directory(filename)
+        
     else:
         print("Sistem eror")
 #__name__
